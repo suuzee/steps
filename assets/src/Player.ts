ea95ba9304a9f72c3f1f7ab89cb7a1f005e466d9
ze.su <1326245718@qq.com>
2019-06-26 21:18:07 +0800

    feat(next):

--- assets/src/Player.ts
+++ assets/src/Player.ts
@@ -13,7 +13,29 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export class Player extends cc.Component {
 
+    private stepDistance: number;
+    private jumpHeight: number;
+    private jumpDuration: number;
+    public canJump: boolean;
+    public index: number;
+
+    public init (stepDistance: number, jumpHeight: number, jumpDuration: number) {
+        this.stepDistance = stepDistance;
+        this.jumpHeight = jumpHeight;
+        this.jumpDuration = jumpDuration;
+        this.canJump = true;
+    }
+
     public jump (step: number) {
+        this.canJump = false;
+        this.index += step;
+        
+        let jumpAction = cc.jumpBy(this.jumpDuration, cc.v2(step * this.stepDistance, 0), this.jumpHeight, 1);
+        let finishAction = cc.callFunc(() => {
+            this.canJump = true;
+        });
+
+        this.node.runAction(cc.sequence(jumpAction, finishAction));
         cc.log(`我跳了${step}步`);
     }
 
--- assets/src/Stage.ts
+++ assets/src/Stage.ts
@@ -10,23 +10,92 @@
 
 import { Game } from './Game';
 import { Player } from './Player';
+import { Block } from './Block';
 
 const {ccclass, property} = cc._decorator;
 
 @ccclass
 export class Stage extends cc.Component {
 
+    @property(cc.Integer)
+    private stepDistance: number = 200;
+    @property(cc.Integer)
+    private jumpHeight: number = 100;
+    @property(cc.Float)
+    private jumpDuration: number = 0.3;
+
     @property(Player)
     private player: Player = null;
-    
+
+    @property(cc.Prefab)
+    private blockPrefab: cc.Prefab = null;
+
+    private lastBlock = true;
+    private lastBlockX = 0;
+    private blockList: Array<Block>;
+
+
     private game: Game = null;
 
     public init (game: Game) {
         this.game = game;
+        this.player.init(this.stepDistance, this.jumpHeight, this.jumpDuration);
+
+        this.blockList = [];
+        this.addBlock(cc.v2(0, 0));
+        for (let i = 0; i < 5; i ++) {
+            this.randomAddBlock();
+        }
     }
 
     public playerJump(step: number) {
-        this.player.jump(step);
+        if (this.player.canJump) {
+            this.player.jump(step);
+            this.moveStage(step);
+            let isDead = !this.hasBlock(this.player.index);
+
+            if (isDead) {
+                cc.log('die');
+                this.game.overGame();
+            } else {
+                this.game.addScore(step === 1 ? 1 : 3);
+            }
+        }
+    }
+
+    private moveStage (step: number) {
+        let moveAction = cc.moveBy(this.jumpDuration, cc.v2(-this.stepDistance * step, 0));
+        this.node.runAction(moveAction);
+        for (let i = 0; i < step; i ++) {
+            this.randomAddBlock();
+        }
+    }
+
+    private randomAddBlock () {
+        if (!this.lastBlock || Math.random() > 0.5) {
+            this.addBlock(cc.v2(this.lastBlockX + this.stepDistance, 0));
+        } else {
+            this.addBlank();
+        }
+
+        this.lastBlockX = this.lastBlockX + this.stepDistance;
+    }
+
+    private addBlock (position: cc.Vec2) {
+        let blockNode = cc.instantiate(this.blockPrefab);
+        this.node.addChild(blockNode);
+        blockNode.position = position;
+        this.blockList.push(blockNode.getComponent(Block));
+        this.lastBlock = true;
+    }
+
+    private addBlank () {
+        this.blockList.push(null);
+        this.lastBlock = false;
+    }
+
+    private hasBlock (index: number): boolean {
+        return this.blockList[index] !== null;
     }
 
 }
